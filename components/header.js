33f9086d3aec31ac3644fc9907b986c823d97c59
Marian <dudychmarian@gmail.com>
2023-06-10 00:03:48 +0100

    styling

--- components/header.js
+++ components/header.js
@@ -6,28 +6,29 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header class="header_container">
-        <h2 class="logo"><a href="/">Dudych</a></h2>
-        <nav class="wrapper">
-          <ul class="nav_link">
-            <li class="zoom"><a href="/" class="nav_link_li">Home</a></li>
-            <li class="zoom"><a href="/about.html" class="nav_link_li">About</a></li>
-            <li class="zoom"><a href="/tech.html" class="nav_link_li">Tech Stack</a></li>
-            <li class="zoom"><a href="/projects.html" class="nav_link_li">Projects</a></li>
-            <li class="zoom"><a href="/contact.html" class="nav_link_li">Contact</a></li>
-          </ul>
-          <ul class="social_link">
-            <li class="zoom">
-              <a href="https://github.com/dudychvo"><img src="./image/github.svg" alt="GitHub" width="{30}" height="{30}" /><a/>
-            </li>
-            <li class="zoom">
-              <img src="./image/email.svg" alt="Email" width="{30}" height="{30}" />
-            </li>
-            <li class="zoom">
-              <img src="./image/linkedin.svg" alt="LinkedIn" width="{30}" height="{30}" />
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <h2 class="logo"><a href="/">Dudych</a></h2>
+      <nav class="wrapper">
+        <ul class="nav_link">
+          <li class="zoom"><a href="/" class="nav_link_li">Home</a></li>
+          <li class="zoom"><a href="/about.html" class="nav_link_li">About</a></li>
+          <li class="zoom"><a href="/cv.html" class="nav_link_li">CV</a></li>
+          <li class="zoom"><a href="/skills.html" class="nav_link_li">Skills</a></li>
+          <li class="zoom"><a href="/units.html" class="nav_link_li">Units</a></li>
+          <li class="zoom"><a href="/contact.html" class="nav_link_li">Contact me</a></li>
+        </ul>
+        <ul class="social_link">
+          <li class="zoom">
+            <a href="https://github.com/dudychvo"><img src="./image/github.svg" alt="GitHub" width="{30}" height="{30}" /></a>
+          </li>
+          <li class="zoom">
+            <a href="https://github.com/dudychvo"><img src="./image/email.svg" alt="Email" width="{30}" height="{30}" /></a>
+          </li>
+          <li class="zoom">
+            <a href="https://github.com/dudychvo"><img src="./image/linkedin.svg" alt="LinkedIn" width="{30}" height="{30}" /></a>
+          </li>
+        </ul>
+      </nav>
+    </header>
     `;
   }
 }
