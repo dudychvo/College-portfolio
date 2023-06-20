db5df453a84d8f688218280afb423b719582b967
Volodymyr <117123903+dudychvo@users.noreply.github.com>
2023-06-20 11:11:05 +0000

    Underline style and text style

--- components/header.js
+++ components/header.js
@@ -9,12 +9,12 @@ class Header extends HTMLElement {
       <h2 class="logo"><img src="./image/logo.svg" alt="logo" height="50px"/></h2>
       <nav class="wrapper">
         <ul class="nav_link">
-          <li class="zoom"><a href="./" class="nav_link_li">Home</a></li>
-          <li class="zoom"><a href="./about.html" class="nav_link_li">About</a></li>
-          <li class="zoom"><a href="./cv.html" class="nav_link_li">CV</a></li>
-          <li class="zoom"><a href="./skills.html" class="nav_link_li">Skills</a></li>
-          <li class="zoom"><a href="./units.html" class="nav_link_li">Units</a></li>
-          <li class="zoom"><a href="./contact.html" class="nav_link_li">Contact me</a></li>
+          <li class="zoom"><a href="./" class="nav_link_li" id="home">Home</a></li>
+          <li class="zoom"><a href="./about.html" class="nav_link_li" id="about">About</a></li>
+          <li class="zoom"><a href="./cv.html" class="nav_link_li" id="cv">CV</a></li>
+          <li class="zoom"><a href="./skills.html" class="nav_link_li" id="skills">Skills</a></li>
+          <li class="zoom"><a href="./units.html" class="nav_link_li" id="units">Units</a></li>
+          <li class="zoom"><a href="./contact.html" class="nav_link_li" id="contact">Contact me</a></li>
         </ul>
         <ul class="social_link">
           <li class="zoom">
